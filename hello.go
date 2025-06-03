bf5ccfa77676d0571cf6de352472fd383f17172f
jimin-kang <jimin.kang821@gmail.com>
2025-06-03 13:53:07 -0500

    added external cmp module

--- hello.go
+++ hello.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	reversed := "nimij"
 	original := morestrings.ReverseRunes(reversed)
 	fmt.Printf("Original: %v, Reversed: %v \n", reversed, original)
+	fmt.Println(cmp.Diff("nimij", "jimin"))
 }
